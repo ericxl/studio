206adfd32e173c3ff8abe7310cba3d5032ced289
Justin Lyan <justin.lyan@utexas.edu>
2018-03-23 17:27:09 -0500

    added calls to the musixmatch api to give information about the song

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,28 @@ $(".show_search").on("click", function(event){
 
     $(".search_area").empty();
 
-    $(".search_area").html('<form class="form-inline"><div class="form-group mb-2"><label for="staticEmail2" class="sr-only">Email</label><input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Search for tutorials: "></div><div class="form-group mx-sm-3 mb-2"><label for="inputPassword2" class="sr-only"></label><input class="form-control" id="inputPassword2" placeholder=" "></div><button type="submit" class="btn btn-primary mb-2 search_btn">Search</button></form>');
+    $(".search_area").html(
+        '<form class="form-inline">' + 
+            '<div class="form-group mb-2">' +
+                '<label for="staticEmail2" class="sr-only">Email</label>' +
+                '<input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Search for tutorials: ">' +
+            '</div>' + 
+            '<div class="form-group mx-sm-3 mb-2">' +
+                '<label for="inputPassword2" class="sr-only"></label>' +
+                '<input class="form-control artist" id="inputPassword2" placeholder="Artist">' +
+            '</div>' + 
+            '<div class="form-group mx-sm-3 mb-2">' + 
+                '<label for="inputPassword2" class="sr-only"></label>' +
+                '<input class="form-control songName" id="inputPassword2" placeholder="Track Name">' +
+            '</div>' +
+            '<button type="submit" class="btn btn-primary mb-2 search_btn">Search</button>' +
+        '<form>'
+    );
 
-    console.log("hello");
     $(".search_btn").on("click", function(event){
         event.preventDefault();
     
         search();
-    
-        console.log('goodbye');
     
     })
 
@@ -20,33 +33,114 @@ $(".show_search").on("click", function(event){
 function search() {
     $("#results").html("");
 
-    var search = $(".form-control").val().trim();
+    var songName = $(".songName").val().trim();
+    var artist = $(".artist").val().trim();
+    var count = 0;
+
+    console.log(artist === "");
 
     $.get(
         "https://www.googleapis.com/youtube/v3/search", {
             part: "snippet, id",
-            q: search + " piano tutorial",
+            q: artist + " " + songName + " piano tutorial",
             type: "video",
             maxResults: 5,
+            videoSyndicated: true,
+            videoEmbeddable: true,
             key: "AIzaSyCDHzzlaYYZ23WOUIkyFB4qVqcgoXu7T1s"
         }, function(data){
             console.log(data);
 
             $.each(data.items, function(i, item){
-                console.log(item);
 
-                var videoID = item.id.videoId;
-                var videoTitle = item.snippet.title;
-                var thumb = item.snippet.thumbnails.medium.url;
-                var description = item.snippet.description;
-
-                console.log(videoID);
-                // $("#results").append("<button onclick='location.href='http://google.com';'><img src= " + thumb + "> " + videoTitle + " " + description + "</button>");
-
-                $("#results").append("<iframe class='tutorial' val='" + videoID + "' width='560' height='315' src='https://www.youtube.com/embed/" + videoID + "' frameborder = '0' allow='autoplay; encrypted media' allowfullscreen></iframe>");
+                var vidInfo = {
+                    videoID: item.id.videoId,
+                    videoTitle: item.snippet.title,
+                    thumb: item.snippet.thumbnails.high.url,
+                    description: item.snippet.description
+                }
                 
+                $("#results").append("<div class='tutorial hello" + count + "' val='" + vidInfo.videoID + "'><img src= " + vidInfo.thumb + " class='image'><p class='title'>" + vidInfo.videoTitle + "</p><p class='description'>" + vidInfo.description + "</p></div>");
+                $(".hello"+count).data(vidInfo);
+                count++;
+
+                $(".tutorial").on("click", function(event){
+                    console.log($(this).data());
+                    var tutVid = $(this).data();
+                    $(".video").html("<iframe class='tutorial_video' val='" + tutVid.videoID + "' width='800' height='500' src='https://www.youtube.com/embed/" + tutVid.videoID + "' frameborder = '0' allow='autoplay; encrypted media' allowfullscreen></iframe>");
+                    
+                })
             });
+            
         }
     )
 
+    $.ajax({
+        type: "GET",
+        data: {
+            apikey:"dc323e0e3f23c8b4bab30839be7c790f",
+            q_track: songName,
+            q_artist: artist,
+            f_has_lyrics: 1,
+            format:"jsonp",
+            callback:"jsonp_callback"
+        },
+        url: "http://api.musixmatch.com/ws/1.1/track.search",
+        dataType: "jsonp",
+        jsonpCallback: 'jsonp_callback',
+        contentType: 'application/json',
+        success: function(data) {
+            var track = data.message.body.track_list[0].track;
+            console.log(track);
+            $(".album_art").html(
+                "<div class='song_info'>" + 
+                    "<div class='artist_name'>" + track.artist_name + "</div>" + 
+                    "<div class='album_name'>" + track.album_name + "</div>" +
+                    '<a href="' + track.track_share_url + '" target="_blank"><button class="btn btn-primary mb-2 search_btn">Lyrics</button></a>' + 
+                "</div>"
+            );
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $(".album_art").empty();
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }    
+      });
+
+      
+
 }
+
+// $.get(
+//     "http://api.musixmatch.com/ws/1.1/track.search?apikey=dc323e0e3f23c8b4bab30839be7c790f", {
+//         q_track: "Let her go",
+//         q_artist: "passenger"
+//     }, function(data){
+//         console.log(data);
+//     }
+// );
+
+// $.ajax({
+//     type: "GET",
+//     data: {
+//         apikey:"dc323e0e3f23c8b4bab30839be7c790f",
+//         q_track:"let her go",
+//         q_artist:"passenger",
+//         f_has_lyrics: 1,
+//         format:"jsonp",
+//         callback:"jsonp_callback"
+//     },
+//     url: "http://api.musixmatch.com/ws/1.1/track.search",
+//     dataType: "jsonp",
+//     jsonpCallback: 'jsonp_callback',
+//     contentType: 'application/json',
+//     success: function(data) {
+//         console.log(data); 
+//     },
+//     error: function(jqXHR, textStatus, errorThrown) {
+//         console.log(jqXHR);
+//         console.log(textStatus);
+//         console.log(errorThrown);
+//     }    
+//   });
