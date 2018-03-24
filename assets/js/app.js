5cfaf1445626cb0b904d5a7b76c44dc3ae1ea269
Justin Lyan <justin.lyan@utexas.edu>
2018-03-23 22:01:32 -0500

    Changed styles and positioning

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ $(".show_search").on("click", function(event){
         '<form class="form-inline">' + 
             '<div class="form-group mb-2">' +
                 '<label for="staticEmail2" class="sr-only">Email</label>' +
-                '<input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Search for tutorials: ">' +
+                '<input type="text" readonly class="form-control-plaintext search_header" id="staticEmail2" value="Search for tutorials:">' +
             '</div>' + 
             '<div class="form-group mx-sm-3 mb-2">' +
                 '<label for="inputPassword2" class="sr-only"></label>' +
@@ -28,8 +28,6 @@ $(".show_search").on("click", function(event){
         
         $(".album_art").empty();
         search();
-        
-    
     })
 
 });
@@ -41,8 +39,6 @@ function search() {
     var artist = $(".artist").val().trim();
     var count = 0;
 
-    console.log(artist === "");
-
     $.get(
         "https://www.googleapis.com/youtube/v3/search", {
             part: "snippet, id",
@@ -53,9 +49,11 @@ function search() {
             videoEmbeddable: true,
             key: "AIzaSyCDHzzlaYYZ23WOUIkyFB4qVqcgoXu7T1s"
         }, function(data){
-            console.log(data);
-
-            $.each(data.items, function(i, item){
+            if(data.items.length === 0){
+                $("#results").text("No results. Please modify your search.");
+            }
+            else {
+                $.each(data.items, function(i, item){
 
                 var vidInfo = {
                     videoID: item.id.videoId,
@@ -69,12 +67,13 @@ function search() {
                 count++;
 
                 $(".tutorial").on("click", function(event){
-                    console.log($(this).data());
                     var tutVid = $(this).data();
                     $(".video").html("<iframe class='tutorial_video' val='" + tutVid.videoID + "' width='800' height='500' src='https://www.youtube.com/embed/" + tutVid.videoID + "' frameborder = '0' allow='autoplay; encrypted media' allowfullscreen></iframe>");
                     
                 })
             });
+            }
+            
             
         }
     )
@@ -95,21 +94,16 @@ function search() {
         contentType: 'application/json',
         success: function(data) {
             var track = data.message.body.track_list[0].track;
-            console.log(track);
             $(".album_art").html(
-                '<div class="card" style="width: 18rem;">' +
+                '<div class="card song_info" style="width: 18rem;">' +
                     '<div class="card-body">' +
                         '<h5 class="card-title">Artist: ' + track.artist_name + '</h5>' +
-                        '<h6 class="card-subtitle mb-2 text-muted">Track: ' + track.track_name + '</h6>' +
+                        '<p class="track_name">Track: ' + track.track_name + '</h6>' +
                         "<p class='card-text album_name'>Album: " + track.album_name + "</p>" +
-                        '<a href="' + track.track_share_url + '" target="_blank"><button class="btn btn-primary mb-2 search_btn">Lyrics</button></a>' +
+                        '<p class="genre">Genre: ' + track.primary_genres.music_genre_list[0].music_genre.music_genre_name + '</p>' + 
+                        '<a href="' + track.track_share_url + '" target="_blank"><button class="btn btn-primary mb-2">Lyrics</button></a>' +
                     '</div>' +
                 '</div>'
-                // "<div class='song_info'>" + 
-                //     "<div class='artist_name'>" + track.artist_name + "</div>" + 
-                //     "<div class='album_name'>" + track.album_name + "</div>" +
-                //     '<a href="' + track.track_share_url + '" target="_blank"><button class="btn btn-primary mb-2 search_btn">Lyrics</button></a>' + 
-                // "</div>"
             );
         },
         error: function(jqXHR, textStatus, errorThrown) {
@@ -123,36 +117,3 @@ function search() {
       
 
 }
-
-// $.get(
-//     "http://api.musixmatch.com/ws/1.1/track.search?apikey=dc323e0e3f23c8b4bab30839be7c790f", {
-//         q_track: "Let her go",
-//         q_artist: "passenger"
-//     }, function(data){
-//         console.log(data);
-//     }
-// );
-
-// $.ajax({
-//     type: "GET",
-//     data: {
-//         apikey:"dc323e0e3f23c8b4bab30839be7c790f",
-//         q_track:"let her go",
-//         q_artist:"passenger",
-//         f_has_lyrics: 1,
-//         format:"jsonp",
-//         callback:"jsonp_callback"
-//     },
-//     url: "http://api.musixmatch.com/ws/1.1/track.search",
-//     dataType: "jsonp",
-//     jsonpCallback: 'jsonp_callback',
-//     contentType: 'application/json',
-//     success: function(data) {
-//         console.log(data); 
-//     },
-//     error: function(jqXHR, textStatus, errorThrown) {
-//         console.log(jqXHR);
-//         console.log(textStatus);
-//         console.log(errorThrown);
-//     }    
-//   });
